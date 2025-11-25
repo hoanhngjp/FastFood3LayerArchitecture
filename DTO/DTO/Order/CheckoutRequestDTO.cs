fbff2aa036e849a7835b2c6c0c3203bec68a3fb9
hoanhngjp <hoanhnghiep2704@gmail.com>
2025-11-26 01:24:39 +0700

    feat(order): implement checkout from cart logic

---
+++ DTO/DTO/Order/CheckoutRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.DTO.Order
+{
+    public class CheckoutRequestDTO
+    {
+        public int AdrsID { get; set; }
+        public int RestaurantID { get; set; }
+        // public string? Note { get; set; } // Ghi chú đơn hàng (nếu cần)
+    }
+}
